01c20792b73a4370cf6f52f9ab2fe2f51196c47a
aaron22 <aaron.gray22@gmail.com>
2012-03-21 15:03:44 -0400

    completed the first couple programs, ending with animation

---
+++ chapter2/catanimation.py
@@ -0,0 +1,50 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+FPS = 1 # frames per second setting
+fpsClock = pygame.time.Clock()
+
+# set up the window
+DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+pygame.display.set_caption('Animation')
+
+WHITE = (255, 255, 255)
+catImg = pygame.image.load('cat.png')
+catx = 10
+caty = 10
+direction = 'right'
+
+while True: # main game loop
+    # clear the screen
+    DISPLAYSURF.fill(WHITE)
+    # get the cat's new postion
+    if direction == 'right':
+        catx += 5
+        if catx == 280:
+            direction = 'down'
+    elif direction == 'down':
+        caty += 5
+        if caty == 220:
+            direction = 'left'
+    elif direction == 'left':
+        catx -= 5
+        if catx == 10:
+            direction = 'up'
+    elif direction == 'up':
+        caty -= 5
+        if caty == 10:
+            direction = 'right'
+    # put that cat on the display at its new position
+    DISPLAYSURF.blit(catImg, (catx, caty))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    fpsClock.tick(FPS)
+    FPS += 1
+
